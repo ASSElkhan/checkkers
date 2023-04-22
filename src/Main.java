b6d38a8be524c13fc31188e20b3e770daabaccec
user <asselkhanberdibek@gmail.com>
2023-04-22 10:00:50 +0600

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int x = 10;
+        int z = 5;
+        System.out.println(x+z);
     }
 }
