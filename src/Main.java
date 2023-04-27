d3e7d427f1e390066a0f5dc1f019a09de1014bfe
user <asselkhanberdibek@gmail.com>
2023-04-27 16:00:11 +0600

    2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int x = 10;
-        int z = 5;
-        System.out.println(x+z);
+        int z = 6;
+        int y = 5;
+        System.out.println(x+z-y);
     }
 }
